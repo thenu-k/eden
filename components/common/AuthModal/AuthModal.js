f555221f2385ac2f1691c75bf244f3dfb6dcc145
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-12-30 17:59:17 +0000

    added on submit starter logic

--- components/common/AuthModal/AuthModal.js
+++ components/common/AuthModal/AuthModal.js
@@ -1,5 +1,6 @@
 import * as S from './AuthModal.styled'
 import { FaGoogle, FaApple, FaMicrosoft } from "react-icons/fa";
+import { useRef } from 'react';
 
 const AuthModal = ({type}) => {
     var  OutputPackage;
@@ -13,6 +14,24 @@ const AuthModal = ({type}) => {
             title: 'Login', desc: 'Get Back to Where You Left'
         }
     }
+    const emailInputRef = useRef()
+    const passwordInputRef = useRef()
+    const usernameInputRef = useRef()
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (type==='register' ){
+            registerUser(usernameInputRef.current.value, emailInputRef.current.value, passwordInputRef.current.value)
+        }
+        else{
+            loginUser(emailInputRef.current.value, passwordInputRef.current.value)
+        }
+    }
+    const registerUser = (username, email, password) => {
+        console.log(username, email, password)
+    }
+    const loginUser = (email, password) => {
+        console.log(email, password)
+    }
     return (
         <S.AuthModalContainer id='AuthModal' className='center'>
             <div className="authmodal inner">
@@ -20,7 +39,7 @@ const AuthModal = ({type}) => {
                     <h1>{OutputPackage.title}</h1>
                     <p>{OutputPackage.desc}</p>
                 </div>
-                <form action="" className='box-shadow-00'>
+                <form className='box-shadow-00' onSubmit={(e) => handleSubmit(e)}>
                     <div className="alt">
                         <button type='button' className='google box-shadow-01'><FaGoogle size={45}/></button>
                         <button type='button' className='apple box-shadow-01'><FaApple size={45}/></button>
@@ -32,18 +51,18 @@ const AuthModal = ({type}) => {
                                 ? (
                                     <div className="username outer">
                                         <label htmlFor="usernameInput">Username</label>
-                                        <input type="text" placeholder='Username' id='usernameInput'/>
+                                        <input type="text" placeholder='Username' id='usernameInput' ref={usernameInputRef}/>
                                     </div>
                                 )
                                 : null
                         }
                         <div className="email outer">
                             <label htmlFor="emailInput">Email</label>
-                            <input type="text" placeholder='Email' id='emailInput'/>
+                            <input type="text" placeholder='Email' id='emailInput' ref={emailInputRef}/>
                         </div>
                         <div className="password outer">
                             <label htmlFor="passwordInput">Password</label>
-                            <input type="text" placeholder='Password' id='passwordInput'/>
+                            <input type="text" placeholder='Password' id='passwordInput' ref={passwordInputRef}/>
                         </div>
                         <div className="submit">
                             <button type="submit" className='center box-shadow-01'>Submit</button>
