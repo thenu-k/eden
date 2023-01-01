b5241a5e8444a6f3a44e54570e59c9007195b711
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-01-01 18:41:36 +0000

    added try catch logic

--- components/common/SplashScreen/SplashScreen.js
+++ components/common/SplashScreen/SplashScreen.js
@@ -3,11 +3,15 @@ import * as S from './SplashScreen.styled'
 
 const SplashScreen = () => {
     const headingRef = useRef()
-    var nAgt = navigator.userAgent;
-    var verOffset,ix;
-    if ((verOffset=nAgt.indexOf("Safari"))!=-1) {
-        browserName = "Safari";
-        headingRef.current.classList.add('safari')
+    try{
+        var nAgt = navigator.userAgent;
+        var verOffset;
+        if ((verOffset=nAgt.indexOf("Safari"))!=-1) {
+            browserName = "Safari";
+            headingRef.current.classList.add('safari')
+        }
+    }catch(e){
+        console.log(e)
     }
     return (
         <S.SplashScreenContainet className='center'>
