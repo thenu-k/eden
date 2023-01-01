b1c30cb13056f576870b8cee61842ffb60581816
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-01-01 00:10:56 +0000

    implemented get note data logic

--- pages/note/[noteID].js
+++ pages/note/[noteID].js
@@ -1,6 +1,34 @@
+import { collection, getDocs, query, where } from 'firebase/firestore';
 import Head from 'next/head'
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { useAuth } from '../../components/auth/authContext';
+import { db } from '../../firebase/firebase';
 
 export default function Home() {
+  const [noteData, setNoteData] = useState(null)
+  const router = useRouter()
+  const {user} = useAuth()
+  const getNote = async() => {
+    const noteID = router.query['noteID']
+    const q = query(collection(db, "notes"), where('__name__', "==", noteID), where('uid','==',user.uid));
+    try{
+      const rawData = await getDocs(q);
+      const result = rawData.docs.map(doc => ({
+        noteID: doc.id,
+        title: doc.data().title,
+        description: doc.data().description,
+        content: doc.data().content,
+        created: doc.data().created
+      })) 
+      setNoteData(result[0])
+    }catch(err){
+      console.log(err)
+    }
+  }
+  useEffect(() => {
+    getNote()
+  },[])
   return (
     <>
       <Head>
@@ -9,7 +37,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Note
+      {
+        (noteData!=null)
+          ? <p>{noteData.title}</p>
+          : <>Does Not Exist</>
+      }
     </>
   )
 }
