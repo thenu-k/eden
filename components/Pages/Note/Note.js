990c9e907b2501853138a4b7231823e60cf0cf05
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-01-01 01:25:49 +0000

    created primitive save function

--- components/Pages/Note/Note.js
+++ components/Pages/Note/Note.js
@@ -1,10 +1,45 @@
 import * as S from './Note.styled'
+import { EditorContent, useEditor } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import { useState } from 'react'
+import { collection, doc, updateDoc, where } from 'firebase/firestore'
+import { db } from '../../../firebase/firebase'
 
 const Note = ({noteData}) => {
+    const [content, setContent] = useState(noteData.content)
+    const editor = useEditor({
+        editable: true,
+        extensions: [
+          StarterKit,
+        ],
+        content:content,
+        onUpdate: (({editor}) => {
+            const html = editor.getHTML()
+            setContent(html)
+        })
+    })
+    const save = async() => {
+        const docRef = doc(db, 'notes', noteData.noteID)
+        try{
+            await updateDoc(docRef, {
+                content: content
+            })
+        }catch(err){
+            console.log(err)
+        }
+    }
     return (
         <S.NoteContainer id='Note' className='center'>
             <div className="note inner">
-                {noteData.title}
+                <div className="editorBar box-shadow-01 center">
+                    <div className="inner editorbar">
+                        <button onClick={save}>Save</button>
+                    </div>
+                </div>
+                <div className="textEditor box-shadow-01">
+
+                    <EditorContent editor={editor} />
+                </div>
             </div>
         </S.NoteContainer>
     );
--- components/Pages/Note/Note.styled.js
+++ components/Pages/Note/Note.styled.js
@@ -2,8 +2,59 @@ import styled from "styled-components";
 
 export const NoteContainer = styled.div`
     width: 100%;
-    & .inner{
+    & .inner.note{
         width: 85%;
         height: 100px;
+        display: flex;
+        flex-direction: column; align-items: center;
+        & .editorBar{
+            position: fixed;
+            width: 85%;
+            z-index: 999;
+            top: 80px; 
+            height: 80px;
+            background-color: white;
+            border-radius: 10px;
+            & .inner.editorbar{
+                height: 100%;
+                display: flex; flex-direction: row; align-items: center;
+                width: 95%;
+                & button{
+                    padding: 10px 30px;
+                    border-radius: 5px;
+                    color: white;
+                    font-size: 1.1em; font-weight: 500;
+                    background: linear-gradient(45deg, purple, blue);
+                    cursor: pointer;
+                }
+            }
+        }
+        & .textEditor{
+            width: 100%;
+            margin-top: 120px;
+            background-color: white;
+            border-radius: 10px;
+            display: flex; 
+            flex-direction: column;
+            align-items: center;
+            & h1{
+                width: 95%;
+                margin: 40px 0 20px 0;
+                font-size: 2em; font-weight: 500;
+            }
+            & >div{
+                width: 100%;
+            }
+            & .ProseMirror{
+                box-sizing: border-box;
+                width: 100%;
+                padding: 40px 2.5% 40px 2.5%;
+                min-height: 400px;
+                &:focus{
+                    outline: 0;
+                }
+
+            }
+        }
     }
 `
