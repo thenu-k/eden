1b2435ec1da3dcc449383150f8833e2e50ae05a0
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-01-01 13:05:54 +0000

    added menubar controls

--- components/Pages/Note/Note.js
+++ components/Pages/Note/Note.js
@@ -4,8 +4,55 @@ import StarterKit from '@tiptap/starter-kit'
 import { useState } from 'react'
 import { collection, doc, updateDoc, where } from 'firebase/firestore'
 import { db } from '../../../firebase/firebase'
+import { useRouter } from 'next/router'
+import { FaBold, FaHeading, FaParagraph, FaListUl } from "react-icons/fa";
+
+const MenuBar = ({ editor}) => {
+    if (!editor) {
+      return null
+    }
+    const toggleColor = (button) => {
+      button.classList.toggle('active')
+    }
+    return (
+      <>
+        <button
+          onClick={(e) => {e.preventDefault(); editor.chain().focus().toggleBold().run()}}
+          disabled={
+            !editor.can()
+              .chain()
+              .focus()
+              .toggleBold()
+              .run()
+          }
+          className={editor.isActive('bold') ? 'is-active' : ''}
+        >
+          <FaBold />
+        </button>
+        <button
+          onClick={(e) => {e.preventDefault(); editor.chain().focus().setParagraph().run()}}
+          className={editor.isActive('paragraph') ? 'is-active' : ''}
+        >
+          <FaParagraph/>
+        </button>
+        <button
+          onClick={(e) => {e.preventDefault();editor.chain().focus().toggleHeading({ level: 3 }).run()}}
+          className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
+        >
+          <FaHeading/>
+        </button>
+        <button
+          onClick={(e) => {e.preventDefault(); editor.chain().focus().toggleOrderedList().run()}}
+          className={editor.isActive('orderedList') ? 'is-active' : ''}
+        >
+            <FaListUl/>
+        </button>
+      </>
+    )
+  }
 
 const Note = ({noteData}) => {
+    const router = useRouter()
     const [content, setContent] = useState(noteData.content)
     const editor = useEditor({
         editable: true,
@@ -18,6 +65,9 @@ const Note = ({noteData}) => {
             setContent(html)
         })
     })
+    const back = () => {
+        router.push('/notebook')
+    }
     const save = async() => {
         const docRef = doc(db, 'notes', noteData.noteID)
         try{
@@ -33,7 +83,11 @@ const Note = ({noteData}) => {
             <div className="note inner">
                 <div className="editorBar box-shadow-01 center">
                     <div className="inner editorbar">
-                        <button onClick={save}>Save</button>
+                        <button className="back" onClick={back}>Back</button>
+                        <div className="controls">
+                            <MenuBar editor={editor}/>
+                        </div>
+                        <button onClick={save} className='save'>Save</button>
                     </div>
                 </div>
                 <div className="textEditor box-shadow-01">
--- components/Pages/Note/Note.styled.js
+++ components/Pages/Note/Note.styled.js
@@ -20,12 +20,38 @@ export const NoteContainer = styled.div`
                 display: flex; flex-direction: row; align-items: center;
                 width: 95%;
                 & button{
-                    padding: 10px 30px;
+                    width: 80px;
+                    height: 40px;
                     border-radius: 5px;
                     color: white;
-                    font-size: 1.1em; font-weight: 500;
+                    font-weight: 500;
                     background: linear-gradient(45deg, purple, blue);
                     cursor: pointer;
+                    &.back{
+                        background:black;
+                    }
+                }
+                & .controls{
+                    height: 100%;
+                    flex-grow: 1;
+                    display: flex; align-items: center;
+                    & button{
+                        &:first-of-type{
+                            margin-left: 60px;
+                        }
+                        width: 40px;
+                        margin-right: 20px;
+                        background: none;
+                        background-color: white;
+                        color: black;
+                        display: flex; align-items: center; justify-content: center;
+                        cursor: pointer;
+                        box-shadow: 0px 0px 3px #929292;
+                        &.is-active{
+                            background-color: black;
+                            color: white;
+                        }
+                    }
                 }
             }
         }
@@ -51,7 +77,81 @@ export const NoteContainer = styled.div`
                 padding: 40px 2.5% 40px 2.5%;
                 min-height: 400px;
                 &:focus{
-                    outline: 0;
+                    outline: none;
+                }
+                *{
+                    font-size: 1.1em;
+                }
+
+                > * + * {
+                margin-top: 20px;
+                }
+                p{
+                    font-size: 1.2em;
+                    strong {
+                    font-size: 1.05em;
+                }
+                }
+                ul,
+                ol {
+                padding: 0 1rem;
+                }
+            
+                h1,
+                h2,
+                h3,
+                h4,
+                h5,
+                h6 {
+                line-height: 1.1;
+                }
+
+                h3{
+                    font-size: 1.5em;
+                    line-height: 1.3;
+                    padding: 10px 0;
+                }
+            
+                code {
+                background-color: rgba(#616161, 0.1);
+                color: #616161;
+                }
+            
+                pre {
+                background: #0D0D0D;
+                color: #FFF;
+                font-family: 'JetBrainsMono', monospace;
+                padding: 0.75rem 1rem;
+                border-radius: 0.5rem;
+            
+                code {
+                    color: inherit;
+                    padding: 0;
+                    background: none;
+                    font-size: 0.8rem;
+                }
+                }
+            
+                img {
+                max-width: 100%;
+                height: auto;
+                }
+            
+                blockquote {
+                padding-left: 1rem;
+                border-left: 2px solid rgba(#0D0D0D, 0.1);
+                }
+            
+                hr {
+                border: none;
+                border-top: 2px solid rgba(#0D0D0D, 0.1);
+                margin: 2rem 0;
+                }
+                strong {
+                    font-weight: 500;
+                }
+                ul{
+                    list-style-type: circle;
                 }
 
             }
