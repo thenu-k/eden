64c8412f46905a8a003149816a0bea7acbffd45d
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-01-02 11:17:01 +0000

    scroll behaviour fix attempt

--- components/common/Header/Header.styled.js
+++ components/common/Header/Header.styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const HeaderContainer = styled.header`
     width: 100%;
     height: 60px;
-    background-color: white;
+    background-color: #F8F9FF;
     & a{
         color: black;
         display: flex; justify-content: center; align-items: center;
